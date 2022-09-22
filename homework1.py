3f6478cedc715329f7262597cf1b721d669be612
Administrator <Admin@BY1-WL-608.corp.wargaming.local>
2022-09-22 21:13:11 +0300

    first commit

---
+++ homework1.py
@@ -0,0 +1,23 @@
+Dima = 32
+print("Возраст Димы =", Dima)
+Sasha = 32
+print("Возраст Саши =", Sasha)
+Anna = 35
+print("Возраст Анны =", Anna)
+summa = Dima + Sasha + Anna
+print("Сумма всех возрастов моих друзей =", summa)
+
+srednee_arifmeticheskoe = Dima + Sasha + Anna / 3
+print("Среднее арифметическое возроста моих друзей =", srednee_arifmeticheskoe)
+
+
+x = 17 / 2 * 3 + 2
+print(x)
+x = 2 + 17 / 2 * 3
+print(x)
+x = 19 % 4 + 15 / 2 * 3
+print(x)
+x = (15 + 6) - 10 * 4
+print(x)
+x = 17 / 2 % 2 * 3
+print(x)

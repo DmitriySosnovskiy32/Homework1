44aefd18173f014539e7f62efefcff1f3d81eb09
Administrator <Admin@BY1-WL-608.corp.wargaming.local>
2022-09-23 16:53:51 +0300

    first commit

---
+++ klass.py
@@ -0,0 +1,11 @@
+my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+new_list = set(my_list)
+print(new_list)
+
+my_list2 = my_list[2:5]
+print(my_list2[::-1])
+
+
+sq_side = int(input())
+list1 = [sq_side * 4, sq_side ** 2, (sq_side ** 2 * 2)]
+print(list1)
